8a8614c4385532b5195ffa5c351549f4bfc2050a
MiamiiTravel <49552971+MiamiiTravel@users.noreply.github.com>
2019-05-21 11:46:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,50 @@ from flask import Flask, request, Response
 app = Flask(__name__)  
 
 
-def get_all_users():
-    print(request.args)   
-    pickle_in = open('data.pickle', 'rb')
-    data_new = pickle.load(pickle_in)  
-    js = json.dumps(data_new)  
+def db_read():
+    try:
+        pickle_in = open('data.pickle', 'rb')
+    except IOError:
+        return {}
+    data = pickle.load(pickle_in)  # Загружает объект из файла
     pickle_in.close()
+    return data
+
+
+def db_write(data):
+    pickle_out = open('data.pickle', 'wb')
+    pickle.dump(data, pickle_out)  
+    pickle_out.close()
+
+
+def get_max_id(data):
+    print('get_max_id== ', data)
+    if data:
+        return max(data, key=int)
+    else:
+        return 0
+
+
+def get_all_users():
+    print(request.args)  
+    all_users = db_read()
+    js = json.dumps(all_users) 
+
     return Response(js, status=200, mimetype='application/json')
 
 
+def get_user(id):
+    db = db_read()
+    if id in db:
+        result = db[id]
+        result['id'] = id
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    else:
+        return Response('{"status": "error", "error": "unknown id"}', status=404, mimetype='application/json')
+
+
 def create_user():
-  data = request.data
     try:
-        data = json.loads(request.data)
         data = request.args
     except Exception as e:
         print(e)
@@ -26,49 +57,71 @@ def create_user():
         return Response('{"status": "error","error":"Bad request"}',
                         status=400,
                         mimetype='application/json')
-    pickle_in = open('data.pickle', 'rb')
-    data_new = pickle.load(pickle_in)   
-    data_new['name'] = data['name']
-    data_new['age'] = data['age']
-
-    pickle_out = open('data.pickle', 'wb')
-    pickle.dump(data_new, pickle_out)     
-    pickle_out.close()
+    # example_dict = {'name': 'Ivan', 'age': 33}
+    db = db_read()
+    print('db:  ', db)
+    new_id = get_max_id(db) + 1
+    print('new_id:  ', new_id)
+    db[new_id] = {'name': data['name'], 'age': data['age']}
+    db_write(db)
     return Response('{"Success": "ok"}',
                     status=200,
                     mimetype='application/json')
 
 
-
-def update_user():
-    data = request.data
-    if 'name' not in data or 'age' not in data:
-        return Response('{"status": "error", "error": "Bad request"}', status=400, mimetype='application/json')
-
-    pickle_in = open('data.pickle', 'rb')
-    data_new = pickle.load(pickle_in)
-    [{'id': 1, 'name': '123'}, {'id': 1, 'name': '321'}]
-    for user in data_new:
-        if user['id'] == data['id']:
-            user['name'] = data['name']
-            user['...'] = data['...']
-            
-
-    js = json.dumps(data_new)
-    pickle_in.close()
+def update_user(id):
+    data = request.args
+    if 'name' not in data and 'age' not in data:
+        return Response('{"status": "error", "error": "age of name missing"}', status=400, mimetype='application/json')
+    db = db_read()
+    if id not in db:
+        return Response('{"status": "error", "error": "unknown id"}', status=404, mimetype='application/json')
+    if 'name' in data:
+        db[id]['name'] = data['name']
+    if 'age' in data:
+        db[id]['age'] = data['age']
+    db_write(db)
     return Response('{"status": "ok"}', status=200, mimetype='application/json')
 
 
+def remove_user(id):
+    db = db_read()
+    if id not in db:
+        return Response('{"status": "error", "error": "unknown id"}', status=404, mimetype='application/json')
+    del db[id]
+    db_write(db)
+    return Response('{"status": "ok"}', status=200, mimetype='application/json')
 
-@app.route('/users/', methods=['GET', 'POST'])
-@app.route('/users/<id>', methods=['GET', 'POST'])
-def users(*args, **kwargs):
+
+def clear_users():
+    db_write({})
+    return Response('{"status": "ok"}', status=200, mimetype='application/json')
+
+
+@app.route('/users/', methods=['GET', 'POST', 'DELETE'])
+def users():
     if request.method == 'GET':
         return get_all_users()
+    elif request.method == 'DELETE':
+        return clear_users()
     elif request.method == 'POST':
         return create_user()
+    else:
+        pass
+
+
+@app.route('/users/<id>', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def user(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return Response('{"status": "error", "error": "invalid id"}', status=400, mimetype='application/json')
+    if request.method == 'GET':
+        return get_user(id)
     elif request.method == 'PATCH':
-        return update_user()
+        return update_user(id)
+    elif request.method == 'DELETE':
+        return remove_user(id)
     else:
         pass
 
